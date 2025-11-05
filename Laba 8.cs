10d462d4676985aafd11136932780c658cb2795c
Vadim-Falcon <stepanenko190709@gmail.com>
2025-11-05 10:14:11 +0200

    Add files via upload

---
+++ Laba 8.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        int[] arr = { 5, -7, -2, 10, 9, -12, 3 };
+
+        int min = Math.Abs(arr[0]);
+        int max = Math.Abs(arr[0]);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (Math.Abs(arr[i]) < min)
+                min = Math.Abs(arr[i]);
+
+            if (Math.Abs(arr[i]) > max)
+                max = Math.Abs(arr[i]);
+        }
+
+        int riz = max - min;
+
+        Console.WriteLine("Найменший за модулем елемент: " + min);
+        Console.WriteLine("Найбільший за модулем елемент: " + max);
+        Console.WriteLine("Різниця між ними: " + riz);
+    }
+}
